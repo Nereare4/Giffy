5c1cda88de94690d17d8e4d9567c7c601a75c05e
nerea <nerea.ramos@storimake.com>
2023-04-27 12:07:11 +0200

    Added the canonical

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import { useCallback } from "react";
-import { useLocation } from "wouter";
+import { Link, useLocation } from "wouter";
 import ListOfGifs from "../components/ListOfGifs";
 import { useGifs } from "../hooks/useGifs";
 import TrendingSearches from "../components/TrendingSearches";
@@ -20,6 +20,7 @@ export default function Home(){
         <>
             <Helmet>
                 <title>Home | Giffy</title>
+                <link rel="canocical" href="https://giffy-project-nre.vercel.app/"/>
             </Helmet>
             <header className="o-header">
                 <SearchForm onSubmit={handleSubmit}/>
